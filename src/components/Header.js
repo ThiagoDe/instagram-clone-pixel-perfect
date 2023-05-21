8c260f61b3eb4b90bd2db63977c4b585489d9bda
ThiagoDe <17tawi@gmail.com>
2023-05-21 12:41:42 -0700

    add menu and profile image at the right side of the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
+import { BeakerIcon, HomeIcon } from '@heroicons/react/24/solid'
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* left */}
       <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
         <Image
@@ -15,12 +15,12 @@ export default function Header() {
           className="object-contain"
         />
       </div>
-      <div className="h-24 w-24 relative lg:hidden cursor-pointer">
+      <div className="h-24 w-12 relative lg:hidden cursor-pointer">
         <Image
           alt="insta icon"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
           layout="fill"
-          className="object-contain"
+          className="object-contain "
         />
       </div>
       {/* middle */}
@@ -28,10 +28,22 @@ export default function Header() {
         <div className="absolute top-2 left-2 ">
           <MagnifyingGlassIcon className="h-5 text-gray-500" />
         </div>
-        <input className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md ' type="text" placeholder='Search' />
+        <input
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md "
+          type="text"
+          placeholder="Search"
+        />
       </div>
 
-      <h1>right side</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out cursor-pointer" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          className="h-10 w-10 object-contain rounded-full  "
+          src="/avatar.png"
+          alt="user image"
+        />
+      </div>
     </div>
   )
 }
