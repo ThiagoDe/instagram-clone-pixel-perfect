dc90d635bdc8db0b74905fbe43c5b5d2e5ac5528
ThiagoDe <17tawi@gmail.com>
2023-05-22 00:27:24 -0700

    add sign in google

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Feed from "@/components/Feed";
-import Header from "@/components/Header";
+import Feed from '../components/Feed'
+import Header from "../components/Header";
 
 
 export default function Home() {
   return (
     <div className="bg-gray-50 min-h-screen">
+      
       <Header/>
-      <Feed/>
+      <Feed />
     </div>
   )
 }
