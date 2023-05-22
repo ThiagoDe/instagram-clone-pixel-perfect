f32f2bbcae073192045de762708e07ebcc1c80e3
ThiagoDe <17tawi@gmail.com>
2023-05-21 19:24:03 -0700

    add oauth and firebase configuration and environment variable

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ export default function Header() {
         
         <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
         {/* left */}
-            <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+            <div className="h-24 w-28 ml-6 relative hidden lg:inline-grid cursor-pointer">
                 <Image
                 alt="insta logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+                src="/instagram-logo.png"
                 layout="fill"
                 className="object-contain"
                 />
